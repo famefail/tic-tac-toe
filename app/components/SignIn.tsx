00b02b1070d89b49ad5e2e0b7d4a68e8f2ef8183
Poramed Thienkriengkrai <poramed.thienkriengkrai@apar.com>
2024-10-26 21:42:04 +0700

    first commit

---
+++ app/components/SignIn.tsx
@@ -0,0 +1,11 @@
+import SignInButton from "./SignInButton"
+import React from "react"
+const SignIn = () => {
+    return(
+        <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow">
+            <SignInButton/>
+        </header>
+    )
+}
+
+export default SignIn
